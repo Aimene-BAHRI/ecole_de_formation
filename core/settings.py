5c3dd9624db8c7c4f140d21052130f5afad94eba
Aimene BAHRI <bahri.aimen48@gmail.com>
2022-01-26 20:30:33 +0100

    add domaine name

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['farabi-school.azurewebsites.net', 'localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['elfarabi-school.com', 'farabi-school.azurewebsites.net', 'localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
